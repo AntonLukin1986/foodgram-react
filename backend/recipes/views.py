05a8bc37ab66a4b35c46591eeffc1f55d45eac8b
Антон Лукин <shahter86@mail.ru>
2022-06-12 12:48:31 +0300

    Ревью. Импорты

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-
 from django_filters.rest_framework import DjangoFilterBackend
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
